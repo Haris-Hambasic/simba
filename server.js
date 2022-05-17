8141beb20124461810a3ff1943f4a50a0fb3a6a7
Haris Hambasic <HarisHambasic@Adiss-MacBook.local>
2022-05-17 04:19:43 -0500

    Cleaned up code file.

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 const express = require("express");
-const res = require("express/lib/response");
 const { google } = require('googleapis');
 
 const simba = google.youtube({
     version: 'v3',
     auth: 'AIzaSyD9jsK33LPEyUpoOD9axlqz1Z1QFpE4OxU'
 });
-
-// console.log("Here: ", tr);
 
 const app = express();
 const PORT = 3000;
@@ -21,93 +18,29 @@ const getVideos = async (videoTitle, numVideos=25) => {
         "q": videoTitle
     });
     return search;
-
-        // .then(async (videos)  => {
-            // return await Promise.resolve("<h1>another one</h1>");
-            // console.log("DATA is here:", videos);
-            // return Promise.resolve({
-            //     videos: videos.data.items
-            // });
-            //     `
-            //         <h1>${videoTitle}</h1>
-            //         <ol>
-            //             ${videos.data.items.map((vid) => `
-            //                 <li style="padding: 1rem">    
-            //                     <a href="https://www.youtube.com/watch?v=${vid.id.videoId}">${vid.snippet.title}</a>
-            //                     <p>${vid.snippet.description}<p>
-            //                 </li>
-            //             `).join("")}
-            //         </ol>
-            //     `;
-            // }); ;
 };
 
 app.use(`/simba/get-videos`, async (req, res) => {
-    // console.log(req.query.videoTitle);
     getVideos(req.query.videoTitle, req.query.numVideos)
         .then(val => {
-            console.log(val);
-            res.send(
-                `
-                    <h1>${req.query.videoTitle}</h1>
-                    <ol>
-                        ${val.data.items.map((vid) => `
-                            <li style="padding: 1rem">    
-                                <a href="https://www.youtube.com/watch?v=${vid.id.videoId}">${vid.snippet.title}</a>
-                                <p>${vid.snippet.description}<p>
-                            </li>
-                        `).join("")}
-                    </ol>
-                `
-            );
+            const videos = [];
+            val.data.items.map(video => {
+                videos.push({
+                    videoIframe: `<iframe width="480" height="360" src="https://www.youtube.com/watch?v=${video.id.videoId}"></iframe>`,
+                    videoURL: `https://www.youtube.com/watch?v=${video.id.videoId}`,
+                    videoID: video.id.videoId,
+                    videoTitle: video.snippet.title,
+                    videoDescription: video.snippet.description,
+                    videoThumbnail: {
+                        image: video.snippet.thumbnails.high.url,
+                        width: video.snippet.thumbnails.high.width,
+                        height: video.snippet.thumbnails.high.height
+                    }
+                });
+            });
+
+            res.json(videos);
         });
-    // res.send(_getVideos.then(val => {
-    //     console.log("Val: -->\n", val);
-    //     return val;
-    // }));
-    // simba.search.list({
-    //     "part": [
-    //       "snippet",
-          
-    //     ],
-    //     "maxResults": 25,
-    //     "q": req.query.videoTitle
-    // })
-    //     // .then(res => res.json())
-    //     .then(data  => {
-    //         console.log("DATA is here:", data);
-    //         res.send(
-    //             `
-    //                 <h1>${req.query.videoTitle}</h1>
-    //                 <ol>
-    //                     ${data.data.items.map((vid) => `
-    //                         <li style="padding: 1rem">    
-    //                             <a href="https://www.youtube.com/watch?v=${vid.id.videoId}">${vid.snippet.title}</a>
-    //                             <p>${vid.snippet.description}<p>
-    //                         </li>
-    //                     `).join("")}
-    //                 </ol>
-    //             `
-    //         );
-    //     });
-});
-
-app.use("/", (req, res) => {
-    res.send(
-        `
-        <form action="/simba/get-videos" method="GET">
-            <label>Video Title</label>
-            <input name="videoTitle" id="videoTitle" type="text" placeholder="Video title..." />
-
-            <br />
-            <label>Number of Videos</label>
-            <input name="numVideos" numVideos="numVideos" type="text" placeholder="Number of videos to return..." />
-
-            <br />
-            <button type="submit">Submit</submit>
-        </form>
-        `
-    );
 });
 
 
